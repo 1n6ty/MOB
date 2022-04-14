2034480f191854ba8615fcb22c0739428af0d964
Ilya <hairais@mail.ru>
2022-04-15 02:13:35 +0700

    simple bug fixes

--- app/src/main/java/com/example/mobv2/adapters/PostAdapter.java
+++ app/src/main/java/com/example/mobv2/adapters/PostAdapter.java
@@ -1,24 +1,33 @@
 package com.example.mobv2.adapters;
 
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
-public class PostAdapter extends RecyclerView.Adapter<PostAdapter.PostItem>
+import com.example.mobv2.R;
+import com.example.mobv2.databinding.ItemPostBinding;
+import com.google.android.material.imageview.ShapeableImageView;
+
+public class PostAdapter extends RecyclerView.Adapter<PostAdapter.PostViewHolder>
 {
 
 
     @NonNull
     @Override
-    public PostItem onCreateViewHolder(@NonNull ViewGroup parent, int viewType)
+    public PostViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType)
     {
-        return null;
+        View postItem =
+                LayoutInflater.from(parent.getContext()).inflate(R.layout.item_post, parent, false);
+        return new PostViewHolder(postItem);
     }
 
     @Override
-    public void onBindViewHolder(@NonNull PostItem holder, int position)
+    public void onBindViewHolder(@NonNull PostViewHolder holder, int position)
     {
 
     }
@@ -29,12 +38,23 @@ public class PostAdapter extends RecyclerView.Adapter<PostAdapter.PostItem>
         return 0;
     }
 
-    protected class PostItem extends RecyclerView.ViewHolder
+    protected class PostViewHolder extends RecyclerView.ViewHolder
     {
+        private ShapeableImageView avatar;
+        private TextView fullname;
+        private TextView date;
+        private ImageView menu;
+        private RecyclerView reactions;
 
-        public PostItem(@NonNull View itemView)
+        public PostViewHolder(@NonNull View itemView)
         {
             super(itemView);
+            ItemPostBinding binding = ItemPostBinding.bind(itemView);
+            avatar = binding.avatarPost;
+            fullname = binding.fullnameField;
+            date = binding.dateField;
+            menu = binding.menu;
+            reactions = binding.reactions;
         }
     }
 }
